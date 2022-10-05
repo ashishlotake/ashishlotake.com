36be594bee3ea9ae09bc203202783a18494eb4c9
ashishlotake <ashish@ashishlotake.com>
2022-10-05 17:01:31 +0530

    sitemetadata

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#2d89ef" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
